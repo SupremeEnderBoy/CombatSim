cf922bc14642bbd69dce0406afe2408d5f799739
SupremeEnderBoy <androsh8495@gmail.com>
2021-04-09 23:45:57 +0100

    Patrol script rework
    reworked patrol script need to finish. touched on animations transitions. Using test scene to implement patrol script

---
+++ Assets/_Scripts/AI Behavior/Patrol.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Patrol : MonoBehaviour
+{
+
+    public float speed;
+    private float waitTime;
+    public float startWaitTime;
+    
+    public Transform[] moveSpots;
+    private int randomSpot;
+    [SerializeField] private Animator anim;
+    
+
+    public Quaternion lookAt;
+    private Rigidbody rbody;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        randomSpot = Random.Range(0, moveSpots.Length);
+        anim.GetComponent <Animator>();
+        //lookAt = ChooseDirection();
+        rbody = GetComponent<Rigidbody>();
+        //transform.rotation = Quaternion.LookRotation(moveSpots[randomSpot].forward);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, moveSpots[randomSpot].position, speed * Time.deltaTime);
+        transform.rotation = Quaternion.LookRotation(moveSpots[randomSpot].forward);
+
+
+        if (Vector3.Distance(transform.position, moveSpots[randomSpot].position) < 0.2f)
+        {
+            if (waitTime <= 0)
+            {
+                randomSpot = Random.Range(0, moveSpots.Length);
+                waitTime = startWaitTime;
+                anim.SetBool("Walk", true);
+
+                transform.rotation = Quaternion.LookRotation(moveSpots[randomSpot].forward);
+                
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+                anim.SetBool("Walk", false);
+            }
+        }
+    }
+
+}
